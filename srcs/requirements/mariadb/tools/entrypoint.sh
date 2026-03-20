99c600977d0ae4454ec310d7ff3b2291ee2124e0
Nikappa57 <lorenz.gaudino@gmail.com>
2026-03-20 02:57:24 +0100

    fix: maria db

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
-# check if the database exist
 if [ ! -d "/var/lib/mysql/mysql" ]; then
-    echo "First execution"
-    
-    # Create base tables
-    mysql_install_db --user=mysql --datadir=/var/lib/mysql > /dev/null
+	echo "Initialize mariadb basic tables"
+	mysql_install_db --user=mysql --datadir=/var/lib/mysql > /dev/null
+fi
 
-    # Read secrets
-    DB_PASS=$(cat /run/secrets/db_password)
-    DB_ROOT_PASS=$(cat /run/secrets/db_root_password)
+# check if the database exist
+if [ ! -d "/var/lib/mysql/${MYSQL_DATABASE}" ]; then
+	echo "Initialize mysql tables"
+	
+	if [ ! -f "/run/secrets/db_password" ] || [ ! -f "/run/secrets/db_root_password" ]; then
+		echo "Error: Database secrets not found"
+		exit 1
+	fi
 
-    # define database
-    cat << EOF > /tmp/init.sql
+	# Read secrets
+	DB_PASS=$(cat /run/secrets/db_password)
+	DB_ROOT_PASS=$(cat /run/secrets/db_root_password)
+
+	echo "Creating table \`${MYSQL_DATABASE}\`, user: \`${MYSQL_USER}\`@'%'"
+
+	# define database
+	cat << EOF > /tmp/init.sql
 CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
 CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${DB_PASS}';
 GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%';
@@ -20,11 +29,11 @@ ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASS}';
 FLUSH PRIVILEGES;
 EOF
 
-    echo "execute mariadb..."
-    # execute in foreground, but first call init.sql
-    exec mysqld_safe --init-file=/tmp/init.sql
+	echo "execute mariadb..."
+	# execute in foreground, but first call init.sql
+	exec mysqld_safe --init-file=/tmp/init.sql
 else
-    echo "Database already exist, execute mariadb..."
+	echo "Database already exist, execute mariadb..."
 	# execute in foreground
-    exec mysqld_safe
+	exec mysqld_safe
 fi
